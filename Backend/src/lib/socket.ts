766b7312917ef75347cca98cc7387a6ddee6a471
Karan raj Surya <suryakaranraj072@gmail.com>
2025-12-21 02:30:59 +0530

    resolved cors

--- Backend/src/lib/socket.ts
+++ Backend/src/lib/socket.ts
@@ -7,7 +7,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chatty-chat-app-2g7e.onrender.com"
+    ],
+    credentials: true,
   },
 });
 
